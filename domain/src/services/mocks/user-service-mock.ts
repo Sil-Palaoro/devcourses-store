99b1fda8d31eb66642818624e742f533a465f007
Sil_Palaoro <silvinapalaoro@gmail.com>
2025-10-06 11:28:38 -0300

    Add  other get methods in user-service

--- domain/src/services/mocks/user-service-mock.ts
+++ domain/src/services/mocks/user-service-mock.ts
@@ -1,12 +1,26 @@
-import { SafeUser } from "src/entities";
+import { SafeUser, UserRole } from "src/entities";
 import { dataUsers, emptyDataUsers } from "../../entities/mocks/user-mock";
 
 
 export const userService = {
-        getById: async (id: string) => {
-            return dataUsers.find((user: SafeUser) => user.id == id );
-        },
         getAll: async () => {
             return dataUsers;
+        },
+        getById: async (id: string) => {
+            return dataUsers.find((user: SafeUser) => user.id == id );
+        },        
+        getByRole: async (role: UserRole) => {
+            let usersByRole: SafeUser[] = [];
+            dataUsers.map((user: SafeUser) => {if (user.role == role) {usersByRole.push(user)}});
+            return usersByRole;
+        },
+        getByName: async (name: string) => {
+            return dataUsers.find((user: SafeUser) => user.name == name);
+        },
+        getBySurname: async (surname: string) => {
+            return dataUsers.find((user: SafeUser) => user.surname == surname);
+        },
+        getByEmail: async (email: string) => {
+            return dataUsers.find((user: SafeUser) => user.email == email);
         }
     };
--- domain/src/services/user-service.ts
+++ domain/src/services/user-service.ts
@@ -1,6 +1,9 @@
-import { SafeUser } from "src/entities/user";
+import { SafeUser, UserRole } from "src/entities/user";
+import { Service } from "src/utils/types/service";
 
-export interface UserService {
-    getById: (id: string) => Promise<SafeUser | undefined>;
-    getAll: () => Promise<SafeUser[]>
+export interface UserService extends Service<SafeUser> {
+    getByRole: (role: UserRole) => Promise<SafeUser[]>;
+    getByName: (name: string) => Promise<SafeUser | undefined>;
+    getBySurname: (surname: string) => Promise<SafeUser | undefined>;
+    getByEmail: (email: string) => Promise<SafeUser | undefined>;
 };
