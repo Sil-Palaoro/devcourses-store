1853c2ad4390a6d3e294e52f3ae3986f186a39c7
Sil_Palaoro <silvinapalaoro@gmail.com>
2025-10-06 13:14:49 -0300

    Update index.ts in use cases

--- domain/src/use-cases/index.ts
+++ domain/src/use-cases/index.ts
@@ -4,7 +4,7 @@ export * from "./course/get-courses-by-level.js";
 export * from "./course/get-courses-by-tag.js";
 export * from "./user/get-user.js";
 export * from "./user/get-user-list.js";
-//export * from "./user/get-users-by-role.js";
+export * from "./user/get-users-by-role.js";
 //export * from "./user/get-user-by-name.js";
 //export * from "./user/get-user-by-surname.js";
 export * from "./user/get-user-by-email.js";
