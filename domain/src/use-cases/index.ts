0e54f49e9928f81fe7f195e582a925d8a07f657e
Sil_Palaoro <silvinapalaoro@gmail.com>
2025-10-06 12:03:05 -0300

    Update index.ts in use-cases

--- domain/src/use-cases/index.ts
+++ domain/src/use-cases/index.ts
@@ -1,4 +1,5 @@
 export * from "./course/get-course.js";
 export * from "./course/get-course-list.js";
+export * from "./course/get-courses-by-level.js";
 export * from "./user/get-user.js";
 // export * from "./user/get-user-list.js";
