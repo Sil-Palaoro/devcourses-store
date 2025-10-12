baeb85a981b347ed2d3443131de7d51f0e3c2cb1
Sil_Palaoro <silvinapalaoro@gmail.com>
2025-10-12 18:04:48 -0300

    Add get-category-list use-case

---
+++ domain/src/use-cases/category/get-category-list.test.ts
@@ -0,0 +1,42 @@
+import { describe, expect, test } from "vitest";
+import { getCategoryList } from "./get-category-list";
+import { categoryServiceMock } from "../../services/mocks/category-service-mock";
+
+
+describe("getCategoryList", async () =>{
+
+    test("Should return the list of categories", async () => {
+        const result = await getCategoryList({dependencies: {categoryService: categoryServiceMock}}); 
+        expect(result).toStrictEqual([ 
+            {id: "1",
+             name: "Frontend",
+             description: "Frontend courses", 
+             courses: []  
+            },
+            {id: "2",
+             name: "Backend",
+             description: "Backend courses",  
+             courses: [] 
+            },
+            {id: "3",
+             name: "FullStack",
+            description: "FullStack courses", 
+            courses: [], 
+            },
+            {id: "4",
+             name: "Databases",
+            description: "Databases courses",  
+            courses: [],
+            },
+            {id: "5",
+             name: "DevOps",
+             description: "DevOps courses",  
+             courses: [],  
+            },
+            {id: "6",
+             name: "Testing",
+             description: "Testing courses",  
+             courses: [],  
+            },])
+            })
+});
---
+++ domain/src/use-cases/category/get-category-list.ts
@@ -0,0 +1,16 @@
+import { CategoryService } from "../../services/category-service";
+
+
+interface GetCategoryList {
+    dependencies: {categoryService: CategoryService};
+};
+
+export async function getCategoryList({dependencies}: GetCategoryList) {   
+
+    const allcategories = await dependencies.categoryService.getAll();
+
+    if (allcategories.length === 0) 
+        return new Error("No hay ningún curso");
+
+    return  allcategories;
+};
