f8ef2028a09f17b7fb21cfcc87b69634077d61d4
Sil_Palaoro <silvinapalaoro@gmail.com>
2025-10-12 19:26:24 -0300

    Refactor Service to segregate interfaces in ReadOnly and Mutable services

--- domain/src/utils/types/service.ts
+++ domain/src/utils/types/service.ts
@@ -1,8 +1,15 @@
 import { Entity } from "./entity";
 
-export interface Service<TEntity extends Entity> {
+export interface Service<TEntity extends Entity> 
+    extends ReadOnlyService<TEntity>, 
+    MutableService<TEntity> {};
+
+export interface ReadOnlyService<TEntity extends Entity> {
     getById: (id: string) => Promise<TEntity | undefined>;
     getAll: () => Promise<TEntity[]>;
+};
+
+export interface MutableService<TEntity extends Entity> {
     create: (data: TEntity) => Promise<void>;
-    delete: (id: string) => Promise<void>;
+    delete: (id: string) => Promise<void>;   
 };
