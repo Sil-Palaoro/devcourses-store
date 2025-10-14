d1fb67c626a00244201840d0d85bdad430c3e48f
Sil_Palaoro <silvinapalaoro@gmail.com>
2025-10-14 17:26:58 -0300

    Update index.js for use cases with update courses and users

--- domain/src/use-cases/index.ts
+++ domain/src/use-cases/index.ts
@@ -6,6 +6,7 @@ export * from "./user/get-users-by-name.js";
 export * from "./user/get-users-by-surname.js";
 export * from "./user/get-user-by-email.js";
 export * from "./user/create-user.js";
+export * from "./user/update-user.js";
 export * from "./user/delete-user.js";
 
 //course use-cases
@@ -14,6 +15,7 @@ export * from "./course/get-course-list.js";
 export * from "./course/get-courses-by-level.js";
 export * from "./course/get-courses-by-tag.js";
 export * from "./course/create-course.js";
+export * from "./course/update-course.js";
 export * from "./course/delete-course.js";
 
 //category use-cases
