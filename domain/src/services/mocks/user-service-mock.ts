f6b177f4cebf8cdda390adcb77ae110afa0ab090
Sil_Palaoro <silvinapalaoro@gmail.com>
2025-10-15 22:51:05 -0300

    Refactor user-service-mock for integration tests

--- domain/src/services/mocks/user-service-mock.ts
+++ domain/src/services/mocks/user-service-mock.ts
@@ -5,23 +5,17 @@ import { vi } from "vitest";
 export const userServiceMock = {
         getAll: async () => dataCompleteUsers,
         getById: async (id: string) => dataCompleteUsers.find((u) => u.id == id ),        
-        getByRole: async (role: UserRole) =>
-            dataCompleteUsers.filter((u) => (u.role === role)),
-        getByName: async (name: string) =>
-            dataCompleteUsers.filter((u) => (u.name === name)),
-        getBySurname: async (surname: string) =>
-            dataCompleteUsers.filter((u) => (u.surname === surname)),
-        getByEmail: async (email: string) => 
-            dataCompleteUsers.find((u) => u.email == email),
+        getByRole: async (role: UserRole) => dataCompleteUsers.filter((u) => (u.role === role)),
+        getByName: async (name: string) => dataCompleteUsers.filter((u) => (u.name === name)),
+        getBySurname: async (surname: string) => dataCompleteUsers.filter((u) => (u.surname === surname)),
+        getByEmail: async (email: string) => dataCompleteUsers.find((u) => u.email == email),
         create: vi.fn(async (user:User) => {
             if (user) dataCompleteUsers.push(user); 
         }),
         update: async (id: string, data: Partial<User>) => {
             const user = dataCompleteUsers.find((u) => u.id == id );
             if (!user) return undefined;
-
             Object.assign(user, data);
-
             return user;
         },  
         delete: vi.fn(async (id: string) => {
