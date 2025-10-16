798c7d351e6e714f6e2e5c89ba5beeda6c2f2e26
Sil_Palaoro <silvinapalaoro@gmail.com>
2025-10-15 23:03:28 -0300

    Update index.j for utils

--- domain/src/utils/index.ts
+++ domain/src/utils/index.ts
@@ -1,2 +1,9 @@
 export * from "./types/entity.js";
 export * from "./types/service.js";
+
+export * from "./to-safe.user.js";
+
+export * from "./crypto/compare-passwords.js";
+export * from "./crypto/hash-password.js";
+export * from "./crypto/generate-jwt.js";
+export * from "./crypto/verify-jwt.js";
