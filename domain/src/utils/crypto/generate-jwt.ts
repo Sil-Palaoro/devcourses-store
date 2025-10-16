5fc63d168c9c6129e1f5df26bf9bff00f542632b
Sil_Palaoro <silvinapalaoro@gmail.com>
2025-10-15 23:01:58 -0300

    Add crypto functions in utils: to hash and compare passwords, and to generate and verify jwt

---
+++ domain/src/utils/crypto/generate-jwt.ts
@@ -0,0 +1,7 @@
+import jwt from "jsonwebtoken";
+
+const SECRET_KEY = process.env.JWT_SECRET || "supersecretkey";
+
+export function generateToken(payload: { id: string, role: string }) {
+    return jwt.sign(payload, SECRET_KEY, {expiresIn: "1h"});
+}
