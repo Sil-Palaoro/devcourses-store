74a81dfd96d1543bfb2ffd6dd4256b7fca36a738
Sil_Palaoro <silvinapalaoro@gmail.com>
2025-10-16 08:41:40 -0300

    Fix typo in index.js for /utils

--- domain/src/utils/index.ts
+++ domain/src/utils/index.ts
@@ -1,7 +1,7 @@
 export * from "./types/entity.js";
 export * from "./types/service.js";
 
-export * from "./to-safe.user.js";
+export * from "./to-safe-user.js";
 
 export * from "./crypto/compare-passwords.js";
 export * from "./crypto/hash-password.js";
