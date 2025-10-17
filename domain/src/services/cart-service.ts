ecde76968a9dbefc4d0b571451aa88566d98aa38
Sil_Palaoro <silvinapalaoro@gmail.com>
2025-10-17 17:52:12 -0300

    Updated add-item-to-cart method in cart-service

--- domain/src/services/cart-service.ts
+++ domain/src/services/cart-service.ts
@@ -4,6 +4,6 @@ import { Service } from "../utils/types/service";
 
 export interface CartService extends Service<Cart>{
     getByUserId: (userId: string) => Promise<Cart | undefined>;
-    addItemToCart: (courseId: string, userId: string) => Promise<Cart>;
+    addItemToCart: (courseId: string, userId: string, priceSnapshot: number) => Promise<Cart>;
     removeItemFromCart: (cartItem: CartItem) => Promise<void>;
 };
