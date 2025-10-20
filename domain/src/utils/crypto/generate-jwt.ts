7d544f743a9ddf177b8a7b71950c3c2739460d75
Sil_Palaoro <silvinapalaoro@gmail.com>
2025-10-19 21:37:37 -0300

    Add key to generate token in .env and change name of fallback key

--- domain/src/utils/crypto/generate-jwt.ts
+++ domain/src/utils/crypto/generate-jwt.ts
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 
-const SECRET_KEY = process.env.JWT_SECRET || "supersecretkey";
+const SECRET_KEY = process.env.JWT_SECRET || "fallback_dev_key";
 
 export function generateToken(payload: { id: string, role: string }) {
     return jwt.sign(payload, SECRET_KEY, {expiresIn: "1h"});
