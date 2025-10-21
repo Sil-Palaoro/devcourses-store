65d50dcea2414cb09a89a038f96b54ff4f00f009
Sil_Palaoro <silvinapalaoro@gmail.com>
2025-10-20 21:22:38 -0300

    Add auth to protected user routes

--- apps/backend/src/routes/user.routes.ts
+++ apps/backend/src/routes/user.routes.ts
@@ -1,25 +1,75 @@
 import { Router } from "express";
 import { UserController } from "../controllers/user.controller";
+import { authMiddleware } from "../middlewares/authMiddleware";
+import { authorizeRoles } from "../middlewares/roleMiddleware";
+import { UserRole } from "@devcourses/domain";
+
 
 const router = Router();
 
-router.get("/", UserController.getAllUsers);
+const ADMIN: UserRole = "admin";
+const INSTRUCTOR: UserRole = "instructor";
+const STUDENT: UserRole = "student";
 
-router.get("/by-role", UserController.getByRole);
-router.get("/by-name", UserController.getByName);
-router.get("/by-surname", UserController.getBySurname);
-router.get("/by-email", UserController.getByEmail);
+//GET
+router.get(
+    "/", 
+    authMiddleware, 
+    authorizeRoles(ADMIN), 
+    UserController.getAllUsers
+);
 
-router.get('/:id', UserController.getUserById);
+router.get(
+    "/by-role", 
+    authMiddleware, 
+    authorizeRoles(ADMIN), 
+    UserController.getByRole);
 
+router.get(
+    "/by-name", 
+    authMiddleware, 
+    authorizeRoles(ADMIN), 
+    UserController.getByName);
+router.get(
+    "/by-surname", 
+    authMiddleware, 
+    authorizeRoles(ADMIN), 
+    UserController.getBySurname);
+router.get(
+    "/by-email", 
+    authMiddleware, 
+    authorizeRoles(ADMIN),     
+    UserController.getByEmail);
+
+router.get(
+    '/:id', 
+    authMiddleware, 
+    authorizeRoles(ADMIN), 
+    UserController.getUserById);
+
+    
+//POST
 router.post("/", UserController.createUser);
 router.post("/register", UserController.registerUser);
 router.post("/login", UserController.loginUser);
 
 
-router.patch('/update/:id', UserController.updateUser);
+//PATCH
+router.patch(
+    '/update/:id',
+    authMiddleware, 
+    authorizeRoles(ADMIN), 
+    UserController.updateUser
+);
 
-router.delete('/delete/:id', UserController.deleteUser);
+
+//DELETE
+router.delete(
+    '/delete/:id',
+    authMiddleware, 
+    authorizeRoles(ADMIN), 
+    UserController.deleteUser
+);
 
 
 export default router;
