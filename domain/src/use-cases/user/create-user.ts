e5fd8d8f7b1d47aa9667ab961354f6c0d14f4311
Sil_Palaoro <silvinapalaoro@gmail.com>
2025-10-20 22:00:42 -0300

    Refactor createUser use case to hash password

--- domain/src/use-cases/user/create-user.ts
+++ domain/src/use-cases/user/create-user.ts
@@ -1,5 +1,7 @@
 import { User, CreateUserDTO } from "../../entities/user";
 import { UserService } from "../../services/user-service";
+import { hashPassword } from "../../utils/crypto/hash-password";
+
 
 interface CreateUserData {
     dependencies: {userService: UserService};
@@ -9,9 +11,13 @@ interface CreateUserData {
 export async function createUser({dependencies, payload}: CreateUserData) {   
     const { v4: uuid } = await import("uuid");
 
+    const hashed = await hashPassword(payload.password);
+    
+
     const user: User = {
         ...payload,
         id: uuid(),
+        password: hashed,
         role: payload.role ?? "student",
         createdAt: new Date(),
         updatedAt: new Date(),
