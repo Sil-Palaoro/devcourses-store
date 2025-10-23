bd7784f616a89ae3011127c44f52d16660401902
Sil_Palaoro <silvinapalaoro@gmail.com>
2025-10-23 08:34:09 -0300

    Add NavBar component first structure and fix bug in App.tsx

---
+++ apps/frontend/devcourses/src/components/NavBar.stories.tsx
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+
+import NavBar from "./NavBar";
+
+
+const meta = {
+    component: NavBar,    
+} satisfies Meta<typeof NavBar>;
+
+export default meta;
+
+// type Story = StoryObj<typeof meta>;
+
---
+++ apps/frontend/devcourses/src/components/NavBar.tsx
@@ -0,0 +1,35 @@
+"use client"
+import Link from "react";
+
+function NavBar() {
+    return (
+        <>
+            <header>
+                <h1>
+                    <Link href="/" title="Devcourses Store">
+                      Devcourses Store
+                    </Link>
+                </h1>
+
+                <nav>
+                    <ul >
+                        <li >
+                          <Link title="Home" href="/">Home</Link>
+                        </li>          
+                        <li>
+                          <Link href="#projects" title="Projects" ></Link>
+                        </li>
+                        <li>
+                          <Link href="#skills" title="Skills"></Link>
+                        </li>
+                        <li>
+                          <Link href="#about" title="About"></Link>
+                        </li>     
+                    </ul>
+                </nav>
+            </header>
+        </>
+    );
+};
+
+export default NavBar;
