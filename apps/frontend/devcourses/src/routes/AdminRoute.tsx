33ada04e23c482883dedf0fe7d25b775776c9c45
Sil_Palaoro <silvinapalaoro@gmail.com>
2025-10-25 11:00:52 -0300

    Fix  react types bug

--- apps/frontend/devcourses/src/routes/AdminRoute.tsx
+++ apps/frontend/devcourses/src/routes/AdminRoute.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 
-export const AdminRoute: React.FC<{ children: JSX.Element }> = ({ children }) => {
+export const AdminRoute: React.FC<{ children: React.JSX.Element }> = ({ children }) => {
     const { isAuthenticated, isAdmin, loading } = useAuth();
     if(loading) return <div>Loading...</div>;
     if(!isAuthenticated) return <Navigate to="/login" replace />
--- apps/frontend/devcourses/src/routes/ProtectedRoute.tsx
+++ apps/frontend/devcourses/src/routes/ProtectedRoute.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 
-export const ProtectedRoute: React.FC<{ children: JSX.Element }> = ({ children }) => {
+export const ProtectedRoute: React.FC<{ children: React.JSX.Element }> = ({ children }) => {
     const { isAuthenticated, loading } = useAuth();
     if(loading) return <div>Loading...</div>;
     return isAuthenticated? children: <Navigate to="/login" replace />; 
