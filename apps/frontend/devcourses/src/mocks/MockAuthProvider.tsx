3fa1b7dfd1664d5357936d25e73da6d5db69e753
Sil_Palaoro <silvinapalaoro@gmail.com>
2025-10-25 11:05:45 -0300

    Add MockAuthProvider to use in stories without using backend

---
+++ apps/frontend/devcourses/src/mocks/MockAuthProvider.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { AuthContext } from "../contexts/AuthContext";
+import { UserRole } from "@devcourses/domain";
+
+type MockAuthProviderProps = {
+    userRole?: UserRole | null;
+    isAuthenticated?: boolean;
+    children: React.ReactNode; 
+}
+
+export const MockAuthProvider: React.FC<MockAuthProviderProps> = ({
+    userRole = null,
+    isAuthenticated = false,
+    children,
+}) => {
+    const value = {
+        userRole,
+        token: isAuthenticated ? "mock-token" : null,
+        loading: false,
+        login: async () => {},
+        logout: () => {},
+        isAuthenticated,
+        isAdmin: userRole === "admin",
+    };
+
+    return (
+        <AuthContext.Provider value={value}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
