4e8fb22d3c62a1dfeebd0e019e15b0ae565757f9
Sil_Palaoro <silvinapalaoro@gmail.com>
2025-10-25 11:06:16 -0300

    Add stories for NavBAr logged out, logged in, and admin

--- apps/frontend/devcourses/src/components/NavBar.stories.tsx
+++ apps/frontend/devcourses/src/components/NavBar.stories.tsx
@@ -1,13 +1,60 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
-
+import { fn } from "storybook/test";
 import NavBar from "./NavBar";
+import { MockAuthProvider } from "../mocks/MockAuthProvider";
 
 
 const meta = {
+    title: 'Components/NavBar',
     component: NavBar,    
+    tags: ['autodocs'],
+    decorators: [
+      (Story) => (
+        <MockAuthProvider>
+            <Story />
+        </MockAuthProvider>
+      )
+    ],
+    parameters: {
+        layout: 'fullscreen',
+      },
+      args: {
+        onLogin: fn(),
+        onLogout: fn(),
+        onCreateAccount: fn(),
+      },
+
 } satisfies Meta<typeof NavBar>;
 
 export default meta;
+type Story = StoryObj<typeof meta>;
 
-// type Story = StoryObj<typeof meta>;
+
+export const LoggedIn: Story = {
+  decorators: [
+     (Story) => (
+        <MockAuthProvider
+        isAuthenticated={true}
+        userRole="student"
+        >
+            <Story />
+        </MockAuthProvider>
+      ) 
+  ],
+};
+
+export const AdminLoggedIn: Story = {
+  decorators: [
+     (Story) => (
+        <MockAuthProvider
+        isAuthenticated={true}
+        userRole="admin"
+        >
+            <Story />
+        </MockAuthProvider>
+      ) 
+  ],
+};
+
+export const LoggedOut: Story = {};
 
