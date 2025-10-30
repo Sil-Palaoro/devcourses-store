3aa9e0e735a5d6356f74c1293f7a7a239d4c3d7b
Sil_Palaoro <silvinapalaoro@gmail.com>
2025-10-30 17:42:00 -0300

    Add CourseList and stories

---
+++ apps/frontend/devcourses/src/components/CourseList.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import CourseList from './CourseList.js';
+import { dataCourses } from "../mocks/course-mock";
+
+
+const meta = {
+  title: 'Components/CourseList',
+  component: CourseList,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof CourseList>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+
+export const Default: Story = {
+    args: {
+        courses: dataCourses
+    }
+};
---
+++ apps/frontend/devcourses/src/components/CourseList.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import CourseCard from "./CourseCard";
+import { Course } from "@devcourses/domain";
+
+interface CourseListProps {
+    courses: Course[];
+}
+
+const CourseList: React.FC<CourseListProps> = ({ courses }) => {
+    return (
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+            {courses.map(course => (
+                <CourseCard key={course.id} course={course} />
+            ))}
+        </div>
+    );
+};
+
+export default CourseList;
