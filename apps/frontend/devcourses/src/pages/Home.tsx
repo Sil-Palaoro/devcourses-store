53ad6b889087b69be7f687ee50cdd20c48b7b588
Sil_Palaoro <silvinapalaoro@gmail.com>
2025-10-30 17:43:02 -0300

    Add CourseList to Home

--- apps/frontend/devcourses/src/pages/Home.tsx
+++ apps/frontend/devcourses/src/pages/Home.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import CourseList from "../components/CourseList";
+import { dataCourses } from "../mocks/course-mock";
 
 const Home = () => {
     return (
-        <main>
-            <h1>DevCourses Store</h1>
-            <p>Tus cursos de programación online</p>
+        <main className="max-w-6xl mx-auto py-8 px-4">
+            <h1 className="text-3xl font-bold mb-4">DevCourses Store</h1>
+            <p className="text-gray-400 mb-8">Tus cursos de programación online</p>
+            <CourseList courses={dataCourses} />
         </main>
     );
 };
