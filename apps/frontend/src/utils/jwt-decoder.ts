12b04d0c11b3d01e8c5f079be5c57f952e429533
Sil_Palaoro <silvinapalaoro@gmail.com>
2025-10-31 11:34:08 -0300

    Refactor jwt-decoder to return complete decoded payload

--- apps/frontend/src/utils/jwt-decoder.ts
+++ apps/frontend/src/utils/jwt-decoder.ts
@@ -2,12 +2,12 @@ import { jwtDecode } from "jwt-decode";
 import { UserRole } from "@devcourses/domain";
 
 interface TokenPayload {
-    id: string;
+    userId: string;
     role: UserRole;
 }
 
 export const tokenDecoder = (token: string) => {
     const payload: TokenPayload = jwtDecode(token!);
-    return payload.role;
+    return payload;
 }
 
