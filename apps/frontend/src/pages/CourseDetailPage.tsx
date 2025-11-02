bc6555fdb692816b433d368c8e54393f38588b03
Sil_Palaoro <silvinapalaoro@gmail.com>
2025-11-02 08:43:57 -0300

    Refactor to use courseService and get id from params

--- apps/frontend/src/pages/CourseDetailPage.tsx
+++ apps/frontend/src/pages/CourseDetailPage.tsx
@@ -1,21 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Course } from "@devcourses/domain";
 import { Button } from "../components/Button";
 import { useAuth } from "../contexts/AuthContext";
 import { cartService } from "../services/cartService";
-import { useNavigate } from "react-router-dom";
-
-interface CourseDetailProps {
-    course: Course;
-}
+import { useNavigate, useParams } from "react-router-dom";
+import { courseService } from "../services/courseService";
 
 
-const CourseDetailPage: React.FC<CourseDetailProps> = ({ course }) => {
+const CourseDetailPage: React.FC = () => {
     const { userId, isAuthenticated } = useAuth();
+    const [course, setCourse] = useState<Course | null>(null);
     const [loading, setLoading] = useState(false);
+    const [loadingCourse, setLoadingCourse] = useState(true);
     const [message, setMessage] = useState("");
     const navigate = useNavigate();
+    const { id } = useParams();
 
+    useEffect(() => {
+        const fetchCourse = async () => {
+            try {
+                if (!id) return;
+                const courseData: Course | null = await courseService.getById(id);
+                setCourse(courseData);
+            } catch (err: any) {
+                setMessage("No se pudo obtener el curso")
+            } finally {
+                setLoadingCourse(false);
+            }
+        }
+        fetchCourse();
+    }, [id])
+
+    if (loadingCourse) return <p className="text-center mt-10">Cargando curso...</p>;
+    if (message) return <p className="text-center text-red-500 mt-10">{message}</p>;
+    if (!course) return <p className="text-center text-red-500 mt-10">Curso no encontrado</p>
     
     const addToCart = async () => {
         if (!isAuthenticated || !userId) {
