5c1fb63d305a35ec53f450f80fc310846f13eb9e
Sil_Palaoro <silvinapalaoro@gmail.com>
2025-11-02 08:48:54 -0300

    Refactor to use courseService

--- apps/frontend/src/pages/Home.tsx
+++ apps/frontend/src/pages/Home.tsx
@@ -1,13 +1,19 @@
 import React from "react";
-import CourseList from "../components/CourseList.js";
-import { dataCourses } from "../mocks/course-mock.js";
+import CourseList from "../components/CourseList";
+import { courseService } from "../services/courseService";
+import { Course } from "@devcourses/domain";
 
-const Home = () => {
+const Home: React.FC = async () => {
+    const allCourses: Course[] | null = await courseService.getAllCourses();
+
+    if(!allCourses) return <p className="text-gray-400 mb-4">No hay cursos disponibles</p>
+
+
     return (
         <main className="max-w-6xl mx-auto py-8 px-4">
             <h1 className="text-3xl font-bold mb-4">DevCourses Store</h1>
             <p className="text-gray-400 mb-8">Tus cursos de programación online</p>
-            <CourseList courses={dataCourses} />
+            <CourseList courses={allCourses} />
         </main>
     );
 };
