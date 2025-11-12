81feabbaf0a58019afa9520016628895b0bcd611
Sil_Palaoro <silvinapalaoro@gmail.com>
2025-11-11 23:22:27 -0300

    Add courseService in frontend to call api

--- apps/frontend/src/services/courseService.ts
+++ apps/frontend/src/services/courseService.ts
@@ -1,22 +1,30 @@
 import api from "./api";
-import { Course } from "@devcourses/domain";
+import { Course, CourseLevel, Tag, UpdateCourseDTO, CreateCourseDTO } from "@devcourses/domain";
+
 
 interface CourseService {
    getAllCourses: () => Promise<Course[] | null>
-   getById: (courseId: string) => Promise<Course | null>;
-   createCourse: (course: Course) => Promise<unknown>;
+   getCourseById: (courseId: string) => Promise<Course | null>;
+   createCourse: (course: CreateCourseDTO) => Promise<Course>;
+   updateCourse: (payload: UpdateCourseDTO) => Promise<Course | undefined>;
+   deleteCourse: (id: string) => Promise<void>;   
+   getCourseByCourseLevel: (courseLevel: CourseLevel) => Promise<Course[]>;
+   getCourseByTag: (tag: Tag) => Promise<Course | undefined>;
 }
 
 export const courseService: CourseService = {
-     async getAllCourses() {
-        return api.get("/courses")
-     },
+     async getAllCourses() {return api.get("/courses")},
      
-     async getById(courseId) {
-        return api.get(`/courses/${courseId}`)
-     },
+     async getCourseById(courseId) {return api.get(`/courses/${courseId}`)},
 
-     async createCourse(course) {
-        return api.post("/courses", course);
-     },
+     async createCourse(course) {return api.post("/courses", course);},
+
+     async updateCourse(payload) {return api.patch(`/courses/update/${payload.id}`, payload.data)},
+     
+     async deleteCourse(id) {return api.delete(`/courses/delete/${id}`)},
+
+     async getCourseByCourseLevel(courseLevel) {return api.get(`/users/by-course-level/courseLevel?=${courseLevel}`)},
+    
+     async getCourseByTag(tag) {return api.get(`/users/by-tag/tag?=${tag}`)},
+
 }
