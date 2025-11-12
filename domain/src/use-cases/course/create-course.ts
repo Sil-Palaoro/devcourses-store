f6f1649997e44c68b218b884e5b1bd21eae317a1
Sil_Palaoro <silvinapalaoro@gmail.com>
2025-11-11 22:54:23 -0300

    Add default ids for Category and Instructor when creating courses

--- domain/src/use-cases/course/create-course.ts
+++ domain/src/use-cases/course/create-course.ts
@@ -2,8 +2,6 @@ import { Course, CreateCourseDTO } from "../../entities/course";
 import { CourseService } from "../../services/course-service";
 
 
-//TODO: cambiar los uuid() de category e instructor ids por id reales
-
 interface CreateCourseData {
     dependencies: {courseService: CourseService};
     payload: CreateCourseDTO
@@ -15,8 +13,8 @@ export async function createCourse({dependencies, payload}: CreateCourseData) {
     const course: Course = {
         ...payload, 
         id: uuid(),
-        categoryId: payload.categoryId ?? uuid(),
-        instructorId: payload.instructorId ?? uuid(),
+        categoryId: payload.categoryId ?? "cmgpmtr780002tiocm5oeixp0",      //id de Category "Fullstack" por default"
+        instructorId: payload.instructorId ?? "cmgqh46pm0000tidovn73ptd5",  //id de user "Olivia" con role "instructor"
         createdAt: new Date(),
         updatedAt: new Date(),
     };
