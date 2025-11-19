d459f0e789e807007d355a90153bca7e156bacae
Sil_Palaoro <silvinapalaoro@gmail.com>
2025-11-19 12:43:10 -0300

    Fix bug for using new database in new computer

--- domain/src/use-cases/course/create-course.ts
+++ domain/src/use-cases/course/create-course.ts
@@ -13,8 +13,8 @@ export async function createCourse({dependencies, payload}: CreateCourseData) {
     const course: Course = {
         ...payload, 
         id: uuid(),
-        categoryId: payload.categoryId ?? "cmgpmtr780002tiocm5oeixp0",      //id de Category "Fullstack" por default"
-        instructorId: payload.instructorId ?? "cmgqh46pm0000tidovn73ptd5",  //id de user "Olivia" con role "instructor"
+        categoryId: payload.categoryId ?? "4d6d7a53-a691-4015-a59f-4e134daf04da",      //id de Category "Fullstack" por default"
+        instructorId: payload.instructorId ?? "d42e721f-c383-466c-9048-daafa95e8d74",  //id de user "Olivia" con role "instructor"
         createdAt: new Date(),
         updatedAt: new Date(),
     };
