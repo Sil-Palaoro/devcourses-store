0a4d4f1682203e7b45d7dcd87ca744dab66669c9
Sil_Palaoro <silvinapalaoro@gmail.com>
2025-11-20 19:58:22 -0300

    Refactor enrollment service

--- domain/src/services/enrollment-service.ts
+++ domain/src/services/enrollment-service.ts
@@ -1,6 +1,10 @@
 import { Enrollment } from "../entities/enrollment";
-import { Service } from "../utils/types/service";
+import { ReadOnlyService } from "../utils/types/service";
 
-export interface EnrollmentService extends Service<Enrollment>{
+export interface EnrollmentService extends ReadOnlyService<Enrollment>{
     enrollUserInCourse: (userId: string, courseId: string) => Promise<Enrollment | undefined>;
+    getEnrollMentsByUser: (userId: string) => Promise<Enrollment[]>;
+    getEnrollmentsByCourse: (courseId: string) => Promise<Enrollment[]>;
+    updateProgress: (enrollmentId: string, progress: number) => Promise<Enrollment | undefined>;
+    markAsCompleted: (enrollmentId: string) => Promise<Enrollment | undefined>;
 };
