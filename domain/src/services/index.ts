9d51e22ce26756cfa7b81ff1de83c66ad1d8efae
Sil_Palaoro <silvinapalaoro@gmail.com>
2025-11-20 18:49:25 -0300

    Update entities and services index.js

--- domain/src/services/index.ts
+++ domain/src/services/index.ts
@@ -2,3 +2,6 @@ export * from "./course-service.js";
 export * from "./user-service.js";
 export * from "./category-service.js";
 export * from "./cart-service.js";
+export * from "./enrollment-service.js";
+export * from "./order-service.js";
+export * from "./payment-service.js";
