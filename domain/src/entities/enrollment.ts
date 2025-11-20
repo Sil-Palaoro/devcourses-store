cc9830f5466a7e841bb773070af4a4a0ad95e37e
Sil_Palaoro <silvinapalaoro@gmail.com>
2025-11-20 19:58:06 -0300

    Refactor enrollment entity

--- domain/src/entities/enrollment.ts
+++ domain/src/entities/enrollment.ts
@@ -1,9 +1,18 @@
 import type { Entity } from "../utils/types/entity";
 
+export const EnrollementStatusOptions = {
+    ACTIVE: "active",
+    COMPLETED: "completed",
+    CANCELLED: "cancelled"
+} as const;
+
+export type EnrollmentStatus = (typeof EnrollementStatusOptions)[keyof typeof EnrollementStatusOptions]
+
 export interface Enrollment extends Entity {
     userId: Entity["id"];
     courseId: Entity["id"];
     enrolledAt: Date;
-    progress?: boolean;       //definir el progress
-    completed?: boolean;          
+    progress?: number;       //porcentaje 0-100
+    status: EnrollmentStatus;
+    completedAt?: Date;          
 }
