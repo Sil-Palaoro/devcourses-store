c1dec523739b725d8c2ec35647afd5ab5ecc5ca8
Sil_Palaoro <silvinapalaoro@gmail.com>
2025-11-19 21:07:40 -0300

    Add orderServiceMock for unit tests

---
+++ domain/src/services/mocks/order-service-mock.unit.ts
@@ -0,0 +1,13 @@
+import { vi } from "vitest";
+
+export const orderServiceMockUnit = {
+    getAll: vi.fn(),
+    getById: vi.fn(),
+    createOrder: vi.fn(),
+    completeOrder: vi.fn(),
+    cancelOrder: vi.fn(),
+    refundOrder: vi.fn(),
+    updateStatus: vi.fn(),
+    addItemsToOrder: vi.fn(),
+    getOrdersForUser: vi.fn()
+};
