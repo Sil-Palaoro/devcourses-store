4f0c47459babbb328df5553c2fa0dd7d393c2fa8
Sil_Palaoro <silvinapalaoro@gmail.com>
2025-11-21 00:17:19 -0300

    Add enrollmentService unit mock

---
+++ domain/src/services/mocks/enrollment-service-mock.unit.ts
@@ -0,0 +1,15 @@
+import { vi } from "vitest";
+import { EnrollmentService } from "../enrollment-service";
+
+export const enrollmentServiceMockUnit: EnrollmentService = {
+    getAll: vi.fn(),
+    getById: vi.fn(), 
+    create: vi.fn(),
+    update: vi.fn(),
+    delete: vi.fn(),    
+    enrollUserInCourse: vi.fn(), 
+    getEnrollMentsByUser: vi.fn(), 
+    getEnrollmentsByCourse: vi.fn(), 
+    updateProgress: vi.fn(), 
+    markAsCompleted: vi.fn(), 
+}
