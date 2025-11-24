410a3cd85f15e3b8e3ebf8fb180c048b4e90b10f
Sil_Palaoro <silvinapalaoro@gmail.com>
2025-11-24 18:42:55 -0300

    Add getPaymentData method in ProviderPaymentService domain interface

--- domain/src/services/provider-payment-service.ts
+++ domain/src/services/provider-payment-service.ts
@@ -2,6 +2,6 @@ import { CreatePaymentProviderDTO, PaymentStatus } from "../entities/payment";
 
 export interface ProviderPaymentService {
     createPayment: (data: CreatePaymentProviderDTO) => Promise<{providerPaymentId: string, checkoutUrl?: string}>;
-
+    getPaymentData: (providerPaymentId: string) => Promise<unknown>;
     getPaymentStatus: (providerPaymentId: string) => Promise<PaymentStatus | unknown>;
 }
