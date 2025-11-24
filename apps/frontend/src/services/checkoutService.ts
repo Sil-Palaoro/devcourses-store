4a33759d661263ed2d32b50a92968b5c3e0ee0f0
Sil_Palaoro <silvinapalaoro@gmail.com>
2025-11-24 20:56:37 -0300

    Clean code

--- apps/frontend/src/services/checkoutService.ts
+++ apps/frontend/src/services/checkoutService.ts
@@ -13,30 +13,3 @@ export const checkoutService: FrontendCheckoutService = {
         return await api.post<StartCheckoutResponse>("/checkout");
      },
 }
-
-    // completePayment: (paymentId: string, providerPaymentId?: string) => Promise<Payment | undefined>;
-    // failPayment: (paymentId: string, providerPaymentId?: string) => Promise<Payment | undefined>;
-
-// interface FrontendPaymentService {
-    // createPayment: (data: Payment) => Promise<Payment | undefined>;
-    // completePayment: (paymentId: string, providerPaymentId?: string) => Promise<Payment | undefined>;
-//     failPayment: (paymentId: string, providerPaymentId?: string) => Promise<Payment | undefined>;
-// }
-
-    // async createPayment(data) {
-    //     return api.post("/payments", { data });
-    //  },
-
-    //  async completePayment(paymentId, providerPaymentId) {
-    //     return api.post("/payments/complete", {
-    //         paymentId,
-    //         providerPaymentId
-    //     });
-    //  },
-
-    //  async failPayment(paymentId, providerPaymentId) {
-    //     return api.post("/payments/fail", {
-    //         paymentId,
-    //         providerPaymentId
-    //     })
-    //  },
